aaae6d94cf865587d666ac08dafc51288ab1668f
RuannDutra <rdutrasantos12@gmail.com>
2024-03-15 08:23:50 -0300

    variavel nomes

--- scripts.js
+++ scripts.js
@@ -1,4 +1,13 @@
-var botao = document.getElementById("botao"); botao.addEventListener("click", alertar, false);
-function alertar (event){
-alert("Você clicou no botão!!! "+ event); 
+var botao = document.getElementById("botao");
+botao.addEventListener("click", alertar,false);
+var nome = document.getElementById("nome");
+
+function alertar(event){
+    alert("Voce clicou no botao"!!! + event)
+
+    var numero = 7;
+    var resultado = numero % 2
+    if(resultado = 0){
+        alert("Esse numero é par!");
+    }
 }
